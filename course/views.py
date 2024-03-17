34943c1f037b613d0cdbce77275c645776b1fae7
GNarendar <gollanarendar2004@gmail.com>
2024-03-17 14:39:08 +0530

    updated the checkanswer

--- course/views.py
+++ course/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.utils.html import escape
 from profilepage.models import Profile
 from .models import Answers
+from django.contrib.auth.models import AnonymousUser,User
+
 
 def course2content(request):
     return render(request, "course/course2/content.html")
@@ -26,16 +28,15 @@ def checkanswerc2(request, slug):
         answers = Answers.objects.filter(topic=validslug).first()
         if answers:
             if option1 == answers.que1 and option2 == answers.que2:
-                user_profile = Profile.objects.filter(user=request.user).first()
-                if user_profile is not None:
-                   if user_profile.c2progress < answers.prgrscontribution :
-                     user_profile.c2progress = answers.prgrscontribution
-                     user_profile.save()
-                     messages.success(request, "Well Done! "+user_profile.name + " That was an Awesome response")
-                   else:
-                    messages.success(request, "Well Done! That was an Awesome response Please Login to Save your progress")   
-                
-            else:
+                if isinstance(request.user,AnonymousUser):
+                 messages.warning(request,"Well Done! That was Awesome response Please Login to save your progress")
+                elif isinstance(request.user,User):
+                 user_profile = Profile.objects.filter(user=request.user).first()
+                 if user_profile and user_profile.c2progress < answers.prgrscontribution :
+                    user_profile.c2progress = answers.prgrscontribution
+                    user_profile.save()
+                 messages.success(request, f"Well Done! {user_profile.name} That was an Awesome response")
+            else:  
                 messages.error(request, "Wrong Answers. Try Again!")
         else:
             return redirect("home:error404")
@@ -43,7 +44,7 @@ def checkanswerc2(request, slug):
         return redirect(f"../../course2/{slug}")
 
 def checkanswerc1(request, slug):
-    if request.method == 'POST':
+   if request.method == 'POST':
         option1 = request.POST.get("q1")
         option2 = request.POST.get("q2")
         
@@ -52,16 +53,18 @@ def checkanswerc1(request, slug):
         answers = Answers.objects.filter(topic=validslug).first()
         if answers:
             if option1 == answers.que1 and option2 == answers.que2:
-                user_profile = Profile.objects.filter(user=request.user).first()
-                if user_profile.c1progress < answers.prgrscontribution :
-                  user_profile.c1progress = answers.prgrscontribution
-                  user_profile.save()
-                else:
-                    pass 
-                messages.success(request, "Well Done! That was an Awesome response")
-            else:
+                if isinstance(request.user,AnonymousUser):
+                 messages.warning(request,"Well Done! That was Awesome response Please Login to save your progress")
+                elif isinstance(request.user,User):
+                 user_profile = Profile.objects.filter(user=request.user).first()
+                 if user_profile and user_profile.c1progress < answers.prgrscontribution :
+                    user_profile.c1progress = answers.prgrscontribution
+                    user_profile.save()
+                 messages.success(request, f"Well Done! {user_profile.name} That was an Awesome response")
+            else:  
                 messages.error(request, "Wrong Answers. Try Again!")
         else:
             return redirect("home:error404")
         
         return redirect(f"../../course1/{slug}")
+    
