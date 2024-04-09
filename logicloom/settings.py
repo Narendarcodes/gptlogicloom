cb89cebdfd36ce107528a37d73d7f39909f6038f
narendar <logicloom127@gmail.com>
2024-04-09 15:52:12 +0000

    update

--- logicloom/settings.py
+++ logicloom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^sgl%p*a=3c9y!-*cdrxh8&j(qtkq&lbitjgt$=*hbz#h4i(-=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','logicloom.pythonanywhere.com',]
 
 
 # Application definition
